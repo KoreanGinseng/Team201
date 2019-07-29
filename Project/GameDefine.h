95787111c86d07116551373b5cee179308dcd05f
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2019-07-29 10:29:23 +0900

    add GameScene

---
+++ Project/GameDefine.h
@@ -0,0 +1,23 @@
+/*****************************************************************
+ *	@file			GameDefine.h
+ *	@brief			ゲームの定義ファイル
+ *	@author			井上颯騎
+ *	@date			2019/07/29
+ *****************************************************************/
+#pragma once
+
+/*****************************************************************
+ * @enum tag_SCENENO
+ * シーン番号の列挙
+ *****************************************************************/
+enum tag_SCENENO {
+	SCENENO_TITLE,
+	SCENENO_GAME,
+	SCENENO_GAMECLEAR,
+	SCENENO_GAMEOVER,
+
+	SCENENO_INOUE,
+	SCENENO_FUJIWARA,
+	SCENENO_KIMURA,
+	SCENENO_OONISHI,
+};
