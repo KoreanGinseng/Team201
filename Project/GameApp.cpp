4a55cfbc80384ae1c1519367d8ae1ff3fd634117
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2019-07-29 11:06:48 +0900

    add fizz

--- Project/GameApp.cpp
+++ Project/GameApp.cpp
@@ -7,12 +7,19 @@
 															@date	2014.05.14
 *//**************************************************************************/
 
-//INCLUDE
+//! INCLUDE
 #include	"GameApp.h"
 #include	"GameDefine.h"
 
-//SCENE
-#include	"Scene/Game/Game.h"
+//! SCENE
+#include	"Game/Game.h"
+#include	"GameClear/GameClear.h"
+#include	"GameOver/GameOver.h"
+#include	"Title/Title.h"
+
+//! GLOBAL
+bool				g_bDebug = false;	//! デバッグ表示フラグ
+CSceneBase*			g_pScene = nullptr;	//! シーン格納ポインタ
 
 /*************************************************************************//*!
 		@brief			アプリケーションの初期化
@@ -22,6 +29,15 @@
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Initialize(void){
+	//開始シーン
+	g_pScene = new CGame();			//ゲームシーンからスタート(デバック用)
+	//g_pScene = new CTitle();		//タイトルシーンからスタート
+
+	//シーンの読み込み
+	g_pScene->Load();
+	//シーンの初期化
+	g_pScene->Initialize();
+
 	return TRUE;
 }
 /*************************************************************************//*!
@@ -34,6 +50,44 @@ MofBool CGameApp::Initialize(void){
 MofBool CGameApp::Update(void){
 	//キーの更新
 	g_pInput->RefreshKey();
+
+	//シーンの更新
+	g_pScene->Update();
+
+	//次のシーンに行くとき
+	if (g_pScene->IsEnd()) 
+	{
+		//次のシーンを取得
+		int change = g_pScene->GetNextScene();
+		//古いシーンを削除
+		delete g_pScene;
+		//新しいシーンを取得
+		switch (change) 
+		{
+		case SCENENO_TITLE:
+			g_pScene = new CTitle();
+			break;
+		case SCENENO_GAME:
+			g_pScene = new CGame();
+			break;
+		case SCENENO_GAMECLEAR:
+			g_pScene = new CGameClear();
+			break;
+		case SCENENO_GAMEOVER:
+			g_pScene = new CGameOver();
+			break;
+		}
+		//新しいシーンの読み込みと初期化
+		g_pScene->Load();
+		g_pScene->Initialize();
+	}
+
+	//F1キーでデバッグフラグの切り替え
+	if (g_pInput->IsKeyPush(MOFKEY_F1)) 
+	{
+		g_bDebug = !g_bDebug;
+	}
+
 	return TRUE;
 }
 /*************************************************************************//*!
@@ -49,6 +103,12 @@ MofBool CGameApp::Render(void){
 	//画面のクリア
 	g_pGraphics->ClearTarget(0.0f,0.0f,1.0f,0.0f,1.0f,0);
 
+	//シーンの描画
+	g_pScene->Render();
+
+	//シーンのデバッグ描画
+	if (g_bDebug) g_pScene->RenderDebug();
+
 	//描画の終了
 	g_pGraphics->RenderEnd();
 	return TRUE;
@@ -61,5 +121,16 @@ MofBool CGameApp::Render(void){
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Release(void){
+	//シーンの解放
+	g_pScene->Release();
+
+	//シーンの削除
+	if (g_pScene) 
+	{
+		delete g_pScene;
+		//削除したポインタには0を入れる
+		g_pScene = 0;
+	}
+
 	return TRUE;
 }
