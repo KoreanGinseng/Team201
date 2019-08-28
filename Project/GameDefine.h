94850b818e9fddc448e2bdfb778e85fec633f74d
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2019-08-28 15:31:26 +0900

    updata

--- Project/GameDefine.h
+++ Project/GameDefine.h
@@ -16,13 +16,9 @@
 
 #define		PLAYER_WIDTH	60
 #define		PLAYER_HEIGHT	64
-
 #define		PLAYER_MAXHP	100
-
 #define		PLAYER_MAXLIFE	3
-
 #define		PLAYER_MAXSPEED	8
-
 #define		PLAYER_GRAVITY	0.3f
 
 //! GLOBAL
