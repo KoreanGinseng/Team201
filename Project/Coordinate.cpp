72981c6da3f79d0e08fa67a9840cf42ef6a6f673
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2019-09-15 11:26:30 +0900

    add rectcircle

--- Project/Coordinate.cpp
+++ Project/Coordinate.cpp
@@ -7,3 +7,45 @@ Vector2 WorldTransration(Vector2 cp, Vector2 sp) {
 Vector2 ScreenTransration(Vector2 cp, Vector2 wp) {
 	return Vector2(wp.x - cp.x, wp.y - cp.y);
 }
+
+bool CollisionRectCircle(const CRectangle rect, const CCircle cir) {
+
+	if ((cir.x >= rect.Left) &&
+		(cir.x <= rect.Right) &&
+		(cir.y >= rect.Top - cir.r) &&
+		(cir.y <= rect.Bottom + cir.r)) {
+		return true;
+	}
+	if ((cir.x >= rect.Left - cir.r) &&
+		(cir.x <= rect.Right + cir.r) &&
+		(cir.y >= rect.Top) &&
+		(cir.y <= rect.Bottom)) {
+		return true;
+	}
+	float a = rect.Left - cir.x;
+	float b = rect.Top - cir.y;
+	float c = sqrtf(a * a + b * b);
+	if (c <= cir.r) {
+		return true;
+	}
+	float d = rect.Right - cir.x;
+	float e = rect.Top - cir.y;
+	float f = sqrtf(d * d + e * e);
+	if (f <= cir.r) {
+		return true;
+	}
+	float g = rect.Left - cir.x;
+	float h = rect.Bottom - cir.y;
+	float i = sqrtf(g * g + h * h);
+	if (i <= cir.r) {
+		return true;
+	}
+	float j = rect.Right - cir.x;
+	float k = rect.Bottom - cir.y;
+	float l = sqrtf(j * j + k * k);
+	if (l <= cir.r) {
+		return true;
+	}
+
+	return false;
+}
--- Project/Coordinate.h
+++ Project/Coordinate.h
@@ -3,3 +3,5 @@
 //関数のプロトタイプ宣言
 Vector2 WorldTransration(Vector2 cp, Vector2 sp);
 Vector2 ScreenTransration(Vector2 cp, Vector2 wp);
+
+bool CollisionRectCircle(const CRectangle rect, const CCircle cir);
