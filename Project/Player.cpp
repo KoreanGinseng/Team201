e3c635b5702886f46fbdf7032041f5e02d399d3a
KimuYuu <53463921+KimuYuu@users.noreply.github.com>
2019-10-08 15:01:03 +0900

    add player

--- Project/Player.cpp
+++ Project/Player.cpp
@@ -1,356 +1,518 @@
-/*****************************************************************
- *	@file			Player.cpp
- *	@brief			プレイヤークラス
- *	@author			井上颯騎
- *	@date			2019/09/29
- *****************************************************************/
+#include		"Player.h"
+#include		"GameDefine.h"
 
-//INCLUDE
-#include "Player.h"
+//ゲームパッドからキーボードへの切り替えフラグ
+bool	g_OperationDebug = true;
+//キーボード、ゲームパッドのテスト操作方法の説明の表示フラグ
+bool	g_PadRenderDebug = false;
+bool	g_KeyRenderDebug = false;
 
-//コンストラクタ
-CPlayer::CPlayer(void) :
-m_pTexture(nullptr),
-m_Pos(Vector2(0,0)),
-m_Move(Vector2(0,0)),
-m_Spd(Vector2(0,0)),
-m_bJump(false),
-m_HP(10),
-m_Stock(3)
-{
+CPlayer::CPlayer() : m_SkillTarget(NULL) {
+	//オブジェクト生成時の初期化
+	m_Skillrang = 0.0f;
+
+	m_CoolTime = 0.0f;
+
+	m_Target = 0;
+
+	m_PosX = 0;
+	m_PosY = 0;
+
+	m_MoveX = 0;
+	m_MoveY = 0;
+
+	m_MoveX2 = 0;
+	m_MoveY2 = 0;
+
+	m_Hp = PLAYER_MAXHP;
+	m_Life = PLAYER_MAXLIFE;
+
+	m_bJump = false;
+	m_bPowerUp = false;
+	m_bTrigger = false;
+	m_bSkill = false;
+
+
 }
 
-//デストラクタ
-CPlayer::~CPlayer(void)
-{
+CPlayer::~CPlayer() {
+
+	Release();
+
 }
 
-//初期化
-void CPlayer::Initialize(void)
-{
-	//画像データのセット
-	m_pTexture = g_pAnimManager->GetResource(FileName[0])->GetTexture();
-	//アニメーションデータのセット
-	m_pMotion = g_pAnimManager->GetResource("playerAnim.bin")->GetMotion();
+bool CPlayer::Load() {
+	//HPテクスチャーの読み込み
+	if (!m_HpTexture.Load("Hp.png")) {
+
+		return false;
+
+	}
+
+	return true;
+
+}
+
+void CPlayer::Initialize() {
+	//スキルの範囲を初期化
+	m_Skillrang = 0.0f;
+
+	m_CoolTime = 100.0f;
+
+	m_bTrigger = false;
+
 	//座標の初期化
-	m_Pos = Vector2(g_pGraphics->GetTargetWidth() / 2, 0);
+	m_PosX = g_pGraphics->GetTargetWidth() / 2;
+	m_PosY = 0;
 	//移動量の初期化
-	m_Move = Vector2(0, 0);
-	//移動速度の初期化
-	m_Spd = Vector2(PLAYER_SPEED, PLAYER_JUMPPOW);
+	m_MoveX = 0;
+	m_MoveY = 0;
 	//HPの初期化
-	m_HP = PLAYER_MAXHP;
+	m_Hp = PLAYER_MAXHP;
 	//残機の初期化
-	m_Stock = PLAYER_MAXSTOCK;
+	m_Life = PLAYER_MAXLIFE;
 	//ジャンプフラグの初期化
 	m_bJump = false;
 	//パワーアップフラグの初期化
-	m_bPowUp = false;
-	//スキルの範囲を初期化
-	m_Skillrang = 0.0f;
-	//
-	m_CoolTime = 100.0f;
-	//
+	m_bPowerUp = false;
+	//スキルの発動フラグを初期化
 	m_bTrigger = false;
+
+
 }
 
-//更新
-void CPlayer::Update(void)
-{
-	//コントローラーが接続されているか
-	if (g_pInput->GetGamePadCount())
-	{
-		//コントローラー入力での移動量更新
-		PadOparation();
+void CPlayer::Update() {
+
+
+	//操作の更新
+	Operation();
+	//残機処理の更新
+	LifeDecrease();
+
+
+}
+
+void CPlayer::Operation() {
+
+	//エンターキーを押した場合、ゲームパッドからキーボードに操作を切り替える
+	if (g_pInput->IsKeyPush(MOFKEY_RETURN)) {
+
+		g_OperationDebug = !g_OperationDebug;
 	}
 
-	//キーボード入力での移動量更新
-	KeyOparation();
+	//コントローラーがPCに接続されている場合操作できる
+	if (g_pInput->GetGamePadCount() && g_OperationDebug) {
 
-	//移動
-	Move();
+		g_KeyRenderDebug = false;
 
-	//スキル
+		//スタートボタンを押した場合、テスト操作方法の説明を表示
+		if (g_pGamePad->IsKeyPush(GAMEKEY_START)) {
+
+			g_PadRenderDebug = !g_PadRenderDebug;
+		}
+
+		PadOperation();
+
+
+	}
+	//キーボード操作
+	else {
+
+		g_PadRenderDebug = false;
+
+		//0キーを押した場合、テスト操作方法の説明を表示
+		if (g_pInput->IsKeyPush(MOFKEY_0)) {
+
+			g_KeyRenderDebug = !g_KeyRenderDebug;
+		}
+
+		KeyOperation();
+	}
+
+}
+
+void CPlayer::TestPadOperation() {
+
+	//ダメージのテスト処理
+	if (g_pGamePad->IsKeyPush(GAMEKEY_X)) {
+
+		m_Hp -= 5;
+
+	}//パワーアップのテスト処理
+	else if (g_pGamePad->IsKeyPush(GAMEKEY_Y)) {
+
+		m_bPowerUp = !m_bPowerUp;
+
+	}//エフェクトのテスト処理
+	else if (g_pGamePad->IsKeyPush(GAMEKEY_RB)) {
+
+		g_pEffectManager->Start(m_PosX + PLAYER_WIDTH * 0.5f, m_PosY + PLAYER_HEIGHT * 0.5f, EFC_TEST);
+
+	}//サウンドの再生テスト
+	else if (g_pGamePad->IsKeyPush(GAMEKEY_LB)) {
+
+		g_pSoundManager->Start(SUD_SOUNDBGM, SOUND_BGM);
+
+	}//サウンドのストップテスト1
+	else if (g_pGamePad->IsKeyPush(GAMEKEY_START)) {
+
+		g_pSoundManager->Stop(SUD_SOUNDBGM, STOP_SUDDEN);
+
+	}//サウンドのストップテスト2
+	else if (g_pGamePad->IsKeyPush(GAMEKEY_BACK)) {
+
+		g_pSoundManager->Stop(SUD_SOUNDBGM, STOP_GRADUALLY);
+
+	}
+
+}
+
+void CPlayer::TestKeyOperation() {
+
+	//ダメージのテスト処理
+	if (g_pInput->IsKeyPush(MOFKEY_1)) {
+
+		m_Hp -= 5;
+
+	}//パワーアップのテスト処理
+	else if (g_pInput->IsKeyPush(MOFKEY_2)) {
+
+		m_bPowerUp = !m_bPowerUp;
+
+	}//エフェクトのテスト処理
+	else if (g_pInput->IsKeyPush(MOFKEY_3)) {
+
+		g_pEffectManager->Start(m_PosX + PLAYER_WIDTH * 0.5f, m_PosY + PLAYER_HEIGHT * 0.5f, EFC_TEST);
+
+	}//サウンドの再生テスト
+	else if (g_pInput->IsKeyPush(MOFKEY_4)) {
+
+		g_pSoundManager->Start(SUD_SOUNDBGM, SOUND_BGM);
+
+	}//サウンドのストップテスト1
+	else if (g_pInput->IsKeyPush(MOFKEY_5)) {
+
+		g_pSoundManager->Stop(SUD_SOUNDBGM, STOP_SUDDEN);
+
+	}//サウンドのストップテスト2
+	else if (g_pInput->IsKeyPush(MOFKEY_6)) {
+
+		g_pSoundManager->Stop(SUD_SOUNDBGM, STOP_GRADUALLY);
+
+	}
+	else if (g_pInput->IsKeyPush(MOFKEY_7)) {
+
+		g_pSoundManager->SetVolume(SUD_SOUNDEFFECT, 0.5f);
+
+	}
+	else if (g_pInput->IsKeyPush(MOFKEY_8)) {
+
+		g_pSoundManager->SetVolume(SUD_SOUNDBGM, 0.5f);
+
+	}
+	else if (g_pInput->IsKeyPush(MOFKEY_9)) {
+
+		g_pSoundManager->AllSetVolume(1.0f);
+
+	}
+
+}
+
+void CPlayer::PadOperation() {
+
+	//スキルの更新
 	Skill();
+	//テスト操作用
+	TestPadOperation();
 
-	//アニメーション更新
-	Animation();
-}
-
-//描画
-void CPlayer::Render(Vector2 screenPos)
-{
-	CRectangle dr = m_SrcRect;
-	//反転フラグが立っているとき描画矩形を反転
-	if (m_bReverse)
-	{
-		float tmp = dr.Right;
-		dr.Right = dr.Left;
-		dr.Left = tmp;
-	}
-	m_pTexture->Render(screenPos.x, screenPos.y, dr);
-}
-
-//デバッグ描画
-void CPlayer::RenderDebug(Vector2 screenPos)
-{
-	CGraphicsUtilities::RenderString(10, 200, "%.1f", m_Skillrang);
-	CGraphicsUtilities::RenderString(10, 240, "%.1f", m_CoolTime);
-	CGraphicsUtilities::RenderCircle(screenPos.x + m_SrcRect.GetWidth() / 2, screenPos.y + m_SrcRect.GetHeight() / 2, m_Skillrang, MOF_COLOR_RED);
-	CGraphicsUtilities::RenderRect(screenPos.x + PLAYER_RECTDIS, screenPos.y + PLAYER_RECTDIS,
-		screenPos.x + m_SrcRect.GetWidth() - PLAYER_RECTDIS, screenPos.y + m_SrcRect.GetHeight(), MOF_COLOR_RED);
-}
-
-//解放
-void CPlayer::Release(void)
-{
-
-}
-
-//パッドオペレーション
-void CPlayer::PadOparation(void)
-{
 	//スティックを右か左に倒した場合、倒した方向に移動
-	if (g_pGamePad->GetStickHorizontal() > 0.8f)
-	{
-		MoveAdd(WAY_RIGHT);
+	if (g_pGamePad->GetStickHorizontal() > 0.8f) {
+
+		m_MoveX += 0.3f;
+		m_MoveX2 += 2.5f;
+		if (m_MoveX > PLAYER_MAXSPEED) {
+
+			m_MoveX = PLAYER_MAXSPEED;
+
+		}
+		if (m_MoveX2 > PLAYER_MAXSPEED * 2) {
+
+			m_MoveX2 = PLAYER_MAXSPEED * 2;
+
+		}
 	}
-	else if (g_pGamePad->GetStickHorizontal() < -0.8f)
-	{
-		MoveAdd(WAY_LEFT);
-	}
-	//スティックを離した場合（移動の操作をしていない場合）
-	else
-	{
+	else if (g_pGamePad->GetStickHorizontal() < -0.8f) {
+
+		m_MoveX -= 0.3f;
+		m_MoveX2 -= 2.5f;
+		if (m_MoveX < -PLAYER_MAXSPEED) {
+
+			m_MoveX = -PLAYER_MAXSPEED;
+
+		}
+		if (m_MoveX2 < -PLAYER_MAXSPEED * 2) {
+
+			m_MoveX2 = -PLAYER_MAXSPEED * 2;
+
+		}
+	}//スティックを離した場合（移動の操作をしていない場合）
+	else {
+
 		//移動量が0なら処理に入らない
 		//移動量が存在する場合、徐々に移動量を0にする
-		if (m_Move.x > 0)
-		{
-			MoveSub(WAY_RIGHT);
-		}
-		else if (m_Move.x < 0)
-		{
-			MoveSub(WAY_LEFT);
-		}
-		else
-		{
-			m_bMove = false;
-		}
-	}
+		if (m_MoveX > 0) {
 
-	//Aボタンを押下かつジャンプフラグがたっていない場合ジャンプする
-	if (g_pGamePad->IsKeyPush(GAMEKEY_A) && !m_bJump)
-	{
-		Jump();
-	}
+			m_MoveX -= 0.2f;
 
-	//LTボタンを押した場合、スキルが発動
-	if (g_pGamePad->GetPadState()->lZ > 500 && m_CoolTime > 0.0f)
-	{
-		m_bTrigger = true;
-	}
-	else if (g_pGamePad->GetPadState()->lZ < 1)
-	{
-		m_bTrigger = false;
-	}
-}
+			if (m_MoveX < 0) {
 
-//キーオペレーション
-void CPlayer::KeyOparation(void)
-{
-	//スティックを右か左に倒した場合、倒した方向に移動
-	if (g_pInput->IsKeyHold(MOFKEY_RIGHT))
-	{
-		MoveAdd(WAY_RIGHT);
-	}
-	else if (g_pInput->IsKeyHold(MOFKEY_LEFT)) 
-	{
-		MoveAdd(WAY_LEFT);
-	}
-	//スティックを離した場合（移動の操作をしていない場合）
-	else
-	{
+				m_MoveX = 0;
+			}
+
+		}
+		else if (m_MoveX < 0) {
+
+			m_MoveX += 0.2f;
+
+			if (m_MoveX > 0) {
+
+				m_MoveX = 0;
+			}
+
+		}
 		//移動量が0なら処理に入らない
 		//移動量が存在する場合、徐々に移動量を0にする
-		if (m_Move.x > 0)
-		{
-			MoveSub(WAY_RIGHT);
+		if (m_MoveX2 > 0) {
+
+			m_MoveX2 -= 2.5f;
+
+			if (m_MoveX2 < 0) {
+
+				m_MoveX2 = 0;
+			}
+
 		}
-		else if (m_Move.x < 0) 
-		{
-			MoveSub(WAY_LEFT);
-		}
-		else
-		{
-			m_bMove = false;
+		else if (m_MoveX2 < 0) {
+
+			m_MoveX2 += 2.5f;
+
+			if (m_MoveX2 > 0) {
+
+				m_MoveX2 = 0;
+			}
+
 		}
 	}
+
+	//移動量を座標に加算
+	m_PosX += m_MoveX;
 
 	//Aボタンを押下かつジャンプフラグがたっていない場合ジャンプする
-	if (g_pInput->IsKeyHold(MOFKEY_UP) && !m_bJump)
-	{
-		Jump();
-	}
+	if (g_pGamePad->IsKeyPush(GAMEKEY_A) && !m_bJump) {
 
-	//スペースキーを押した場合、スキルが発動
-	if (g_pInput->IsKeyHold(MOFKEY_SPACE) && m_CoolTime > 0.0f)
-	{
-		m_bTrigger = true;
-	}
-	else if (g_pInput->IsKeyPull(MOFKEY_SPACE))
-	{
-		m_bTrigger = false;
-	}
-}
+		//ジャンプ効果音のテスト処理
+		g_pSoundManager->Start(SUD_SOUNDEFFECT, SOUND_EFFECT);
 
-//移動
-void CPlayer::Move(void)
-{
-	//移動量に重力をかける
-	m_Move.y += GRAVITY;
-	m_Pos += m_Move;
-}
+		m_bJump = true;
 
-//加速処理
-void CPlayer::MoveAdd(WAY w)
-{
-	switch(w)
-	{
-	case WAY_RIGHT:
-		m_bReverse = false;
-		m_Move.x += m_Spd.x;
-		//最大速度を超えないようにする
-		if (m_Move.x > PLAYER_MAXSPEED)
-		{
-			m_Move.x = PLAYER_MAXSPEED;
+		//パワーアップフラグが立っている場合、ジャンプ力上昇
+		if (m_bPowerUp) {
+
+			m_MoveY = PLAYER_HIGHJUMP;
+
 		}
-		break;
+		else {
 
-	case WAY_LEFT:
-		m_bReverse = true;
-		m_Move.x -= m_Spd.x;
-		//最大速度を超えないようにする
-		if (m_Move.x < -PLAYER_MAXSPEED)
-		{
-			m_Move.x = -PLAYER_MAXSPEED;
+			m_MoveY = PLAYER_JUMP;
+
 		}
-		break;
+
 	}
-	m_bMove = true;
+
+	m_MoveY += PLAYER_GRAVITY;
+
+	m_PosY += m_MoveY;
+
+
+
 }
 
-//減速処理
-void CPlayer::MoveSub(WAY w)
-{
-	switch (w)
-	{
-	case WAY_RIGHT:
-		m_Move.x -= m_Spd.x - 0.1f;
-		//ゼロ以下にならないようにする
-		if (m_Move.x < 0)
-		{
-			m_Move.x = 0;
+void CPlayer::KeyOperation() {
+
+	//テスト操作用
+	TestKeyOperation();
+
+	//スティックを右か左に倒した場合、倒した方向に移動
+	if (g_pInput->IsKeyHold(MOFKEY_RIGHT)) {
+
+		m_MoveX += 0.2f;
+		m_MoveX2 += 2.5f;
+		if (m_MoveX > PLAYER_MAXSPEED) {
+
+			m_MoveX = PLAYER_MAXSPEED;
+
 		}
-		break;
-	case WAY_LEFT:
-		m_Move.x += m_Spd.x - 0.1f;
-		//ゼロ以上にならないようにする
-		if (m_Move.x > 0)
-		{
-			m_Move.x = 0;
+		if (m_MoveX2 > PLAYER_MAXSPEED * 2) {
+
+			m_MoveX2 = PLAYER_MAXSPEED * 2;
+
 		}
-		break;
+
 	}
-}
+	else if (g_pInput->IsKeyHold(MOFKEY_LEFT)) {
 
-//アニメーション処理
-void CPlayer::Animation(void)
-{
-	//移動中なら移動モーションにする
-	if(m_bMove && m_pMotion->GetMotionNo() == ANIM_WAIT)
-	{
-		m_pMotion->ChangeMotion(ANIM_MOVE);
-	}
-	if(!m_bMove && m_pMotion->GetMotionNo() == ANIM_MOVE)
-	{
-		m_pMotion->ChangeMotion(ANIM_WAIT);
-	}
+		m_MoveX -= 0.2f;
+		m_MoveX2 -= 2.5f;
 
-	//アニメーション加算
-	m_pMotion->AddTimer(CUtilities::GetFrameSecond());
+		if (m_MoveX < -PLAYER_MAXSPEED) {
 
-	//アニメーション矩形更新
-	m_SrcRect = m_pMotion->GetSrcRect();
-}
+			m_MoveX = -PLAYER_MAXSPEED;
 
-//ジャンプ処理
-void CPlayer::Jump(void)
-{
-	//ジャンプ効果音のテスト処理
-	g_pSoundManager->GetResource(FileName[10])->Play();
+		}
+		if (m_MoveX2 < -PLAYER_MAXSPEED * 2) {
 
-	//ジャンプフラグを立てる
-	m_bJump = true;
+			m_MoveX2 = -PLAYER_MAXSPEED * 2;
 
-	//パワーアップフラグが立っている場合、ジャンプ力上昇
-	if (m_bPowUp)
-	{
-		m_Move.y = PLAYER_HIGHJUMPPOW;
-	}
-	else
-	{
-		m_Move.y = PLAYER_JUMPPOW;
-	}
-}
+		}
 
-//当たり判定
-void CPlayer::CollisionStage(Vector2 o) {
-	m_Pos += o;
-	//落下中の下埋まり、ジャンプ中の上埋まりの場合は移動を初期化
-	if (o.y < 0 && m_Move.y > 0)
-	{
-		m_Move.y = 0;
-		if (m_bJump)
-		{
-			m_bJump = false;
+	}//スティックを離した場合（移動の操作をしていない場合）
+	else {
+
+		//移動量が0なら処理に入らない
+		//移動量が存在する場合、徐々に移動量を0にする
+		if (m_MoveX > 0) {
+
+			m_MoveX -= 0.3f;
+
+			if (m_MoveX < 0) {
+
+				m_MoveX = 0;
+			}
+
+		}
+		else if (m_MoveX < 0) {
+
+			m_MoveX += 0.3f;
+
+			if (m_MoveX > 0) {
+
+				m_MoveX = 0;
+			}
+
+		}
+
+		//移動量が0なら処理に入らない
+		//移動量が存在する場合、徐々に移動量を0にする
+		if (m_MoveX2 > 0) {
+
+			m_MoveX2 -= 2.5f;
+
+			if (m_MoveX2 < 0) {
+
+				m_MoveX2 = 0;
+			}
+
+		}
+		else if (m_MoveX2 < 0) {
+
+			m_MoveX2 += 2.5f;
+
+			if (m_MoveX2 > 0) {
+
+				m_MoveX2 = 0;
+			}
+
 		}
 	}
-	else if (o.y > 0 && m_Move.y < 0)
-	{
-		m_Move.y = 0;
+
+	//移動量を座標に加算
+	m_PosX += m_MoveX;
+
+	//Aボタンを押下かつジャンプフラグがたっていない場合ジャンプする
+	if (g_pInput->IsKeyHold(MOFKEY_UP) && !m_bJump) {
+
+		//ジャンプ効果音のテスト処理
+		g_pSoundManager->Start(SUD_SOUNDEFFECT, SOUND_EFFECT);
+
+		m_bJump = true;
+
+		//パワーアップフラグが立っている場合、ジャンプ力上昇
+		if (m_bPowerUp) {
+
+			m_MoveY = PLAYER_HIGHJUMP;
+
+		}
+		else {
+
+			m_MoveY = PLAYER_JUMP;
+
+		}
+
 	}
-	//左移動中の左埋まり、右移動中の右埋まりの場合は移動を初期化する
-	if ((o.x < 0 && m_Move.x > 0) || (o.x > 0 && m_Move.x < 0))
-	{
-		m_Move.x = 0;
-	}
+
+	m_MoveY += PLAYER_GRAVITY;
+
+	m_PosY += m_MoveY;
+
 }
 
 void CPlayer::Skill() {
 
 	//スキルの円に座標や半径を代入
-	m_SkillCircle.x = m_Pos.x + m_SrcRect.GetWidth() / 2;
-	m_SkillCircle.y = m_Pos.y + m_SrcRect.GetHeight() / 2;
+	m_SkillCircle.x = m_PosX + PLAYER_WIDTH / 2;
+	m_SkillCircle.y = m_PosY + PLAYER_HEIGHT / 2;
 	m_SkillCircle.r = m_Skillrang;
 
+	//LTボタンを押した場合、スキルが発動
+	if (g_pGamePad->GetPadState()->lZ > 500/*&&m_CoolTime>0.0f*/) {
+
+		m_bTrigger = true;
+
+	}
+	else if (g_pGamePad->GetPadState()->lZ < 1) {
+
+		m_bTrigger = false;
+
+	}
+
 	//スキルが発動している場合ターゲットの範囲を広げる
-	if (m_bTrigger)
-	{
+	if (m_bTrigger) {
+
 		m_Skillrang += 10;
-		m_CoolTime -= 0.1f;
-		if (m_Skillrang >= PLAYER_MAXSKILLRANGE)
-		{
+
+		/*m_CoolTime -= 0.1f;*/
+
+		if (m_Skillrang >= PLAYER_MAXSKILLRANGE) {
+
 			m_Skillrang = PLAYER_MAXSKILLRANGE;
+
 		}
 	}
-	else
-	{
-		if (m_Skillrang > 0.0f)
-		{
+	else {
+
+
+		if (m_Skillrang > 0.0f) {
+
 			m_Skillrang = 0.0f;
 			m_Target = 0;
+
+			if (!m_SkillTarget.empty()) {
+
+				for (int i = 0; i < m_SkillTarget.size(); i++) {
+
+					m_SkillTarget[i]->SetTarget(false);
+
+				}
+
+				m_SkillTarget.clear();
+
+			}
+
 		}
+
 	}
+
+
 }
 
 
@@ -509,4 +671,90 @@ void CPlayer::SkillColision(CEnemy* pene, int eneCount, CObject* pobj, int objCo
 	}
 
 
+}
+
+void CPlayer::LifeDecrease() {
+	//HPが0以下になった場合残機を減らしHPを初期値に戻す
+	if (m_Hp <= 0) {
+
+		m_Life--;
+
+		m_Hp = PLAYER_MAXHP;
+	}
+}
+
+void CPlayer::Render(Vector2 scroll) {
+
+	//プレイヤー(仮)の描画
+	CGraphicsUtilities::RenderRect(scroll.x, scroll.y, scroll.x + PLAYER_WIDTH, scroll.y + PLAYER_HEIGHT, MOF_XRGB(255, 0, 0));
+	//プレイヤーのHPの描画
+	RenderState();
+	//デバックの描画
+	RenderDebug();
+
+	CGraphicsUtilities::RenderCircle(scroll.x + PLAYER_WIDTH / 2, scroll.y + PLAYER_WIDTH / 2, m_Skillrang, MOF_COLOR_RED);
+
+}
+
+void CPlayer::RenderState() {
+	//HPの値によってケージの長さを変化
+	CRectangle rec(0, 0, 532 * (m_Hp*0.01f), 64);
+
+	m_HpTexture.Render(248, 150, rec);
+
+
+
+}
+
+void CPlayer::RenderDebug() {
+
+	/*CGraphicsUtilities::RenderString(0, 30, MOF_XRGB(0, 255, 0), "テスト操作方法説明の表示:キーボードの場合0キー : ゲームパッドの場合STRATボタン");*/
+	/*CGraphicsUtilities::RenderString(0, 60, MOF_XRGB(0, 255, 0), "ゲームパッドに接続されている状態でもエンターキーで操作をキーボードと切り替え可能");
+	CGraphicsUtilities::RenderString(0, 90, MOF_XRGB(0, 255, 0), "m_MoveX %.f m_MoveY %.f m_bPowerUp %d m_bJump %d m_Hp %d m_Life %d m_PosX %.f", m_MoveX, m_MoveY, m_bPowerUp, m_bJump, m_Hp, m_Life,m_PosX);
+	*/
+	/*CGraphicsUtilities::RenderString(0, 30, "Effectボリューム%f BGMボリューム%f", g_pSoundManager->RenderDebug(SUD_SOUNDBGM), g_pSoundManager->RenderDebug(SUD_SOUNDEFFECT));*/
+
+	CGraphicsUtilities::RenderString(0, 120, "Target%d ベクトルサイズ%d", m_Target, m_SkillTarget.size());
+	if (g_KeyRenderDebug) {
+
+		CGraphicsUtilities::RenderString(0, 120, MOF_XRGB(255, 0, 0), "1キーで疑似ダメージ: 2キーで強化: 3キーでエフェクト: 4キーでBGM再生: 5キーでBGM急停止: ");
+		CGraphicsUtilities::RenderString(0, 150, MOF_XRGB(255, 0, 0), "6キーでBGM徐々に停止");
+	}
+
+	if (g_PadRenderDebug) {
+
+		CGraphicsUtilities::RenderString(0, 120, MOF_XRGB(255, 0, 0), "スティックで移動: Aボタンでジャンプ: Xボタンで疑似ダメージ Yボタンで強化: RBボタンでエフェクト:");
+		CGraphicsUtilities::RenderString(0, 150, MOF_XRGB(255, 0, 0), " LBボタンでBGM再生: STARTボタンでBGM急停止: BACKボタンでBGM徐々に停止");
+
+	}
+
+
+}
+
+void CPlayer::Release() {
+	//HPテクスチャー(仮)の解放
+	m_HpTexture.Release();
+}
+
+void CPlayer::CollisionStage(Vector2 o) {
+	m_PosX += o.x;
+	m_PosY += o.y;
+	//落下中の下埋まり、ジャンプ中の上埋まりの場合は移動を初期化
+	if (o.y < 0 && m_MoveY > 0)
+	{
+		m_MoveY = 0;
+		if (m_bJump)
+		{
+			m_bJump = false;
+		}
+	}
+	else if (o.y > 0 && m_MoveY < 0)
+	{
+		m_MoveY = 0;
+	}
+	//左移動中の左埋まり、右移動中の右埋まりの場合は移動を初期化する
+	if ((o.x < 0 && m_MoveX>0) || (o.x > 0 && m_MoveX < 0))
+	{
+		m_MoveX = 0;
+	}
 }
