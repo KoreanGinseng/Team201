385b8e81bf09bef6c8710008edc88a4a01a0f3e9
karakaru <53463850+karakaru@users.noreply.github.com>
2019-10-18 17:49:53 +0900

    add release

--- Project/WinMain.cpp
+++ Project/WinMain.cpp
@@ -2,7 +2,7 @@
 
 int WINAPI _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
 {
-	_CrtSetBreakAlloc(1314);
+	//_CrtSetBreakAlloc(1314);
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 
 	Mof::LPFramework pFrame = new Mof::CDX11GameFramework(); 
