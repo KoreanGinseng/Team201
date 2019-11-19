acab8c31b242350c357d5393717fbb50e6d70c1e
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2019-11-19 15:44:36 +0900

    adadadadada

--- newnew/Project/WinMain.cpp
+++ newnew/Project/WinMain.cpp
@@ -5,7 +5,6 @@ CImeProc* pp = new CImeProc();
 int WINAPI _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
 {
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
-	_CrtSetBreakAlloc(1865);
 	Mof::LPFramework pFrame = new Mof::CDX11GameFramework(); 
 	Mof::WINDOWSGAMEFRAMEWORKINITIALIZEINFO Info;
 	Info.WindowCreateInfo.pProc = pp;
