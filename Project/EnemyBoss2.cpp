9d63ae888bc2cf0e4373930873e4844d126de6e5
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-01-13 07:37:40 +0900

    Update EnemyBoss2.cpp

--- Project/EnemyBoss2.cpp
+++ Project/EnemyBoss2.cpp
@@ -2,11 +2,14 @@
 
 
 
-CEnemyBoss2::CEnemyBoss2()
+CEnemyBoss2::CEnemyBoss2(void) :
+CEnemy()
 {
+	m_EnemyType = ENEMY_BOSS_2;
+	m_bSelectTarget = false;
 }
 
 
-CEnemyBoss2::~CEnemyBoss2()
+CEnemyBoss2::~CEnemyBoss2(void)
 {
 }
