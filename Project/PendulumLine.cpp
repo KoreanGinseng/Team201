d6749904bd52c9548aaaf14f16f00998e7d4a113
fujiwaratakumi373 <53463955+fujiwaratakumi373@users.noreply.github.com>
2020-01-16 11:39:13 +0900

    del

--- Project/PendulumLine.cpp
+++ Project/PendulumLine.cpp
@@ -27,45 +27,6 @@ bool CPendulumLine::Load(void)
 	bool a = false, b = false;
 void CPendulumLine::Update(const bool & me)
 {	
-	////振り子の計算
-	////重力加速度1エネルギー
-
-	////加速する
-	//deg += LineSpd * 10.1f;
-	////反転する
-	//if (deg < 0 && LineSpd < 0)
-	//{
-	//	LineSpd *= -1;
-	//	a = true;
-	//}
-	//if (deg < -90 && b)
-	//{
-	//	b = true;
-	//}
-	//if (deg > 0 && LineSpd > 0 && b)
-	//{
-	//	LineSpd *= -1;
-	//	b = false;
-	//	a = false;
-	//}
-	//MOF_PRINTLOG("%.f\n", deg);
-
-	////LineSpd += 0.1;
-	////if (LineSpd > 1)
-	////{
-	////	LineSpd = 1;
-	////}
-	////deg += LineSpd;
-	//
-	////d(振り子の角度)が90度になると反転する
-
-	////if (deg > 90|| deg < -90)
-	////{
-	////	deg *= -1;
-	////	LineSpd *= -1;
-	////}
-	//m_Rote = MOF_ToRadian(deg);
-
 	m_CosTime+= m_CosSpd;
 	float c = cos(m_CosTime);
 	m_Rote = MOF_ToRadian(80) * c;//copysignf((c * c), c);
